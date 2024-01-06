08eec71f45ddb264bf81d462f81533aa3365c61b
SAEHA <sumaiyasaeha@gmail.com>
2024-01-07 00:42:08 +0600

    created model and user schema

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+app.listen(3000, () => {
+   console.log('Server listening on 3000');
+})
+// const mongoose = require('mongoose');
+// mongoose.connect("mongodb+srv://aronnok:aronnok@cluster0.yufjo2r.mongodb.net/?retryWrites=true&w=majority", () => {
+//    console.log('Connected to Mongo DB Successfully!!');
+// })
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://aronnok:aronnok@cluster0.yufjo2r.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
---
+++ models/user.js
@@ -0,0 +1,92 @@
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+var userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+      maxlength: 32,
+    },
+    lastname: {
+      type: String,
+      maxlength: 32,
+    },
+    username: {
+        type: String,
+        unique: true,
+        required:true,
+        maxlength: 32,
+      },
+      dob: {
+        type: Date,
+        maxlength: 12,
+      },
+      religion: {
+        type: String,
+        maxlength: 32,
+      },
+      mobile: {
+        type: Number,
+        required:true,
+        maxlength: 11,
+      },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    present_addr: {
+      type: String,
+      maxlength:100,
+      trim: true
+    },
+    encry_password: {
+      type: String,
+      required: true
+    },
+    // salt: String,
+    // role: {
+    //   type: Number,
+    //   default: 0
+    // },
+    // purchases: {
+    //   type: Array,
+    //   default: []
+    // }
+  }
+//   ,
+//   { timestamps: true }
+);
+
+// userSchema
+//   .virtual("password")
+//   .set(function(password) {
+//     this._password = password;
+//     this.salt = uuidv1();
+//     this.encry_password = this.securePassword(password);
+//   })
+//   .get(function() {
+//     return this._password;
+//   });
+
+// userSchema.methods = {
+//   autheticate: function(plainpassword) {
+//     return this.securePassword(plainpassword) === this.encry_password;
+//   },
+
+//   securePassword: function(plainpassword) {
+//     if (!plainpassword) return "";
+//     try {
+//       return crypto
+//         .createHmac("sha256", this.salt)
+//         .update(plainpassword)
+//         .digest("hex");
+//     } catch (err) {
+//       return "";
+//     }
+//   }
+// };
+
+module.exports = mongoose.model("User", userSchema);
