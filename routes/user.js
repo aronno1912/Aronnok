9942adc88cd541e4a52e045ecc40a266182bcf0a
SAEHA <sumaiyasaeha@gmail.com>
2024-01-10 20:29:34 +0600

    completed user route and controller

--- routes/user.js
+++ routes/user.js
@@ -1,36 +1,24 @@
 const express = require("express");
 const router = express.Router();
-// const User = require('../models/user.js'); 
-// // const bcrypt = require('bcrypt');
-// router.post('/user', async (request, response) => {//new endpoint /user, Any POST request to the /user URL will be handled by this router
-      
-//       try {
-//         const user = new User({
-//           firstname : request.body.firstname,
-//           lastname : request.body.lastname,
-//           username : request.body.username,
-//           dob : request.body.dob,
-//           religion : request.body.religion,
-//           mobile:request.body.mobile,
-//           present_addr:request.body.present_addr,
-//           password : request.body.password,
-//           email : request.body.email
-//        });
-//         await user.save();
-//       //   user.save(function (err) {
-//       //     if (err) {
-//       //         console.log(err);
-//       //     } else {
-//       //       response.status(201).json({ message: 'User signed up successfully!' });
-//       //     }
-//       // });
-//       response.status(201).json({ message: 'User signed up successfully!' });
-//         // You may want to handle the response or redirect to a login page
-        
-//       } catch (error) {
-//         console.error(error);
-//         response.status(500).json({ error: 'Internal Server Error' });
-//       }
-// });
-module.exports = router;
 
+const {
+  getUserById,
+  getUser,
+  updateUser,
+  userPurchaseList,
+} = require("../controller/user");
+const { isSignedIn, isAuthenticated } = require("../controller/auth");
+
+router.param("userId", getUserById);
+
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+
+// router.get(
+//   "/orders/user/:userId",
+//   isSignedIn,
+//   isAuthenticated,
+//   userPurchaseList
+// );
+
+module.exports = router;
