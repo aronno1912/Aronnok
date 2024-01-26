f506552b728e3a00522befc7d4f72010ca5af09a
aronno1912 <debjanyaronno@gmail.com>
2024-01-27 00:57:46 +0600

    one order details

---
+++ frontend/src/Pages/OrderDetailsAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from '../Components/Sidebar/Sidebar';
+
+const OrderDetailsAdmin = () => {
+    return (
+        <div>
+            <Sidebar/>
+        </div>
+    );
+};
+
+export default OrderDetailsAdmin;
