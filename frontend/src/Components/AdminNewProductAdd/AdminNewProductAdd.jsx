51448ec3560cfd6cbfc9e2ae270386a92fc8ffe8
aronno1912 <debjanyaronno@gmail.com>
2024-01-28 03:41:36 +0600

    need to fix in addnewprod photo

--- frontend/src/Components/AdminNewProductAdd/AdminNewProductAdd.jsx
+++ frontend/src/Components/AdminNewProductAdd/AdminNewProductAdd.jsx
@@ -1,5 +1,176 @@
 
 
+// import React, { useState } from 'react';
+// import './AdminNewProductAdd.css';
+// import axios from 'axios';
+
+// const AdminNewProductAdd = () => {
+//   const [productInfo, setProductInfo] = useState({
+//     name: '',
+//     description: '',
+//     category: '',
+//     stock: 0,
+//     sold: 0,
+//     tag: '',
+//     price: 0,
+//     photo: null,
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setProductInfo((prevInfo) => ({
+//       ...prevInfo,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleFileChange = (e) => {
+//     const file = e.target.files[0];
+//     setProductInfo((prevInfo) => ({
+//       ...prevInfo,
+//       photo: file,
+//     }));
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     // Implement your logic to handle form submission
+  
+//             try {
+//                 const formData = new FormData();
+//                 for (const key in productInfo) {
+//                     formData.append(key, productInfo[key]);
+//                 }
+
+//                 const response = await axios.post('http://localhost:8000/api/product/create/65a294c44865e9f4138c7281', formData, {
+//                     headers: {
+//                         'Content-Type': 'multipart/form-data',
+//                     },
+//                 });
+//                 console.log('Form submitted:', productInfo);
+//                 // Reset form fields if needed
+//                 alert('New product added successfully!');
+//                 setProductInfo({
+//                     name: '',
+//                     description: '',
+//                     category: '',
+//                     stock: 0,
+//                     sold: 0,
+//                     tag: '',
+//                     price: 0,
+//                     photo: null,
+//                 });
+//             } catch (error) {
+//                 console.error('Error adding product:', error);
+//             }
+//         };
+
+//   return (
+//     <div className="admin-add-product-form">
+//       <form onSubmit={handleSubmit}>
+//         <div className="form-left">
+//           <label htmlFor="name">Product Name</label>
+//           <input
+//             type="text"
+//             id="name"
+//             name="name"
+//             value={productInfo.name}
+//             onChange={handleChange}
+//             required
+//           />
+
+         
+
+//           <label htmlFor="category">Category</label>
+//           <select className='ab'
+//             id="category"
+//             name="category"
+//             value={productInfo.category}
+//             onChange={handleChange}
+//             required
+//           >
+//             <option value="">Select Category</option>
+//             <option value="Bansai">Bansai</option>
+//             <option value="Cactus">Cactus</option>
+//             <option value="Hybrid Fruits">Hybrid Fruits</option>
+//             <option value="Hybrid Flowers">Hybrid Flowers</option>
+//           </select>
+
+//           <label htmlFor="description">Description</label>
+          
+//           <input
+//             type="text"
+//             id="description"
+//             name="description"
+//             value={productInfo.description}
+//             onChange={handleChange}
+//             required
+//           />
+//         </div>
+
+
+//         <div className="form-right">
+//           <label htmlFor="stock">Stock Quantity</label>
+//           <input
+//             type="number"
+//             id="stock"
+//             name="stock"
+//             value={productInfo.stock}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           <label htmlFor="sold">Sold</label>
+//           <input
+//             type="number"
+//             id="sold"
+//             name="sold"
+//             value={productInfo.sold}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           <label htmlFor="tag">Tag</label>
+//           <input
+//             type="text"
+//             id="tag"
+//             name="tag"
+//             value={productInfo.tag}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           <label htmlFor="price">Price</label>
+//           <input
+//             type="number"
+//             id="price"
+//             name="price"
+//             value={productInfo.price}
+//             onChange={handleChange}
+//             required
+//           />
+
+//           <label htmlFor="photo">Upload Photo</label>
+//           <input
+//             type="file"
+//             id="photo"
+//             name="photo"
+//             accept="image/*"
+//             onChange={handleFileChange}
+//             required
+//           />
+
+//           <button className='addProductAdminButton' type="submit">Add Product</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AdminNewProductAdd;
+
+
+// // AdminAddProductForm.jsx
 import React, { useState } from 'react';
 import './AdminNewProductAdd.css';
 import axios from 'axios';
@@ -34,131 +205,70 @@ const AdminNewProductAdd = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Implement your logic to handle form submission
-  
-            try {
-                const formData = new FormData();
-                for (const key in productInfo) {
-                    formData.append(key, productInfo[key]);
-                }
 
-                const response = await axios.post('http://localhost:8000/api/product/create/65a294c44865e9f4138c7281', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                });
-                console.log('Form submitted:', productInfo);
-                // Reset form fields if needed
-                alert('New product added successfully!');
-                setProductInfo({
-                    name: '',
-                    description: '',
-                    category: '',
-                    stock: 0,
-                    sold: 0,
-                    tag: '',
-                    price: 0,
-                    photo: null,
-                });
-            } catch (error) {
-                console.error('Error adding product:', error);
-            }
-        };
+    try {
+      const formData = new FormData();
+      for (const key in productInfo) {
+        formData.append(key, productInfo[key]);
+      }
+
+      await axios.post('http://localhost:8000/api/product/create/65a294c44865e9f4138c7281', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      alert('New product added successfully!');
+      setProductInfo({
+        name: '',
+        description: '',
+        category: '',
+        stock: 0,
+        sold: 0,
+        tag: '',
+        price: 0,
+        photo: null,
+      });
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
+  };
 
   return (
     <div className="admin-add-product-form">
       <form onSubmit={handleSubmit}>
         <div className="form-left">
           <label htmlFor="name">Product Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={productInfo.name}
-            onChange={handleChange}
-            required
-          />
-
-         
+          <input type="text" id="name" name="name" value={productInfo.name} onChange={handleChange} required />
 
           <label htmlFor="category">Category</label>
-          <select className='ab'
-            id="category"
-            name="category"
-            value={productInfo.category}
-            onChange={handleChange}
-            required
-          >
+          <select className='ab' id="category" name="category" value={productInfo.category} onChange={handleChange} required>
             <option value="">Select Category</option>
-            <option value="Bansai">Bansai</option>
+            <option value="Bonsai">Bonsai</option>
             <option value="Cactus">Cactus</option>
             <option value="Hybrid Fruits">Hybrid Fruits</option>
             <option value="Hybrid Flowers">Hybrid Flowers</option>
           </select>
 
           <label htmlFor="description">Description</label>
-          
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={productInfo.description}
-            onChange={handleChange}
-            required
-          />
+          <input type="text" id="description" name="description" value={productInfo.description} onChange={handleChange} required />
         </div>
-
 
         <div className="form-right">
           <label htmlFor="stock">Stock Quantity</label>
-          <input
-            type="number"
-            id="stock"
-            name="stock"
-            value={productInfo.stock}
-            onChange={handleChange}
-            required
-          />
+          <input type="number" id="stock" name="stock" value={productInfo.stock} onChange={handleChange} required />
 
           <label htmlFor="sold">Sold</label>
-          <input
-            type="number"
-            id="sold"
-            name="sold"
-            value={productInfo.sold}
-            onChange={handleChange}
-            required
-          />
+          <input type="number" id="sold" name="sold" value={productInfo.sold} onChange={handleChange} required />
 
           <label htmlFor="tag">Tag</label>
-          <input
-            type="text"
-            id="tag"
-            name="tag"
-            value={productInfo.tag}
-            onChange={handleChange}
-            required
-          />
+          <input type="text" id="tag" name="tag" value={productInfo.tag} onChange={handleChange} required />
 
           <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            value={productInfo.price}
-            onChange={handleChange}
-            required
-          />
+          <input type="number" id="price" name="price" value={productInfo.price} onChange={handleChange} required />
 
           <label htmlFor="photo">Upload Photo</label>
-          <input
-            type="file"
-            id="photo"
-            name="photo"
-            accept="image/*"
-            onChange={handleFileChange}
-            required
-          />
+          <input type="file" id="photo" name="photo" accept="image/*" onChange={handleFileChange} required />
 
           <button className='addProductAdminButton' type="submit">Add Product</button>
         </div>
@@ -168,7 +278,4 @@ const AdminNewProductAdd = () => {
 };
 
 export default AdminNewProductAdd;
-
-
-// AdminAddProductForm.jsx
 
