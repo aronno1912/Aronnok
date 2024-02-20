687cad7557774d002ecff5279ac7226603d9371c
aronno1912 <debjanyaronno@gmail.com>
2024-02-21 01:24:58 +0600

    getallreqWorks

--- routes/sell.js
+++ routes/sell.js
@@ -4,9 +4,11 @@ const { check, validationResult, oneOf } = require("express-validator");
 const{
     sellRequest,
     requestApproval,
+    getAllSellRequest,
 }= require('../controller/sell');
 const { getUserById } = require("../controller/user");
 router.param("userId", getUserById);
 router.post('/sellRequest/:userId',sellRequest);
 router.post('/requestApproval/:reqId',requestApproval);
+router.get('/sellRequest/getAll',getAllSellRequest);
 module.exports = router;
